4219ac2b823f80054155919782892e8c1c1aa1f6
Calvin Féau <calvin@calvins-mbp.lan>
2019-05-03 00:08:19 -0700

    pseudocode details

--- js/main.js
+++ js/main.js
@@ -1,82 +1,104 @@
 
-// WAR GAME - Score version
+// _____________________________________________________________
+// CONSTANTS IN THE GLOBAL SCOPE
 
 
-// ---------------------------------------------------------------------------------------------------------------------------
-// INTRODUCTION 
-
-// Each player plays a card by clicking on his/her deck
-// When a player wins a round, he/she adds the difference of value to his/her score
-// The player with the highest score at the end wins!
-
-// ---------------------------------------------------------------------------------------------------------------------------    
-// BOARD GAME
-
-// The title will be displayed as a header
-
-// The board game will be displayed in 3 columns:
-// Both edge columns will include the Player's name, deck and live score
-
-// The middle column will be the battle field where:
-// On each side, the player's card being played would be display
-// On the bottom, the player winning the round will be displayed
-// When the game ends, instead of the player winning the round, the war winner will be displayed 
-
-// A reset button will also be included
-
-
-// ---------------------------------------------------------------------------------------------------------------------------
-// INITIALIZATION 
-
-// The users will write (input) their name where "Player 1"/"Player 2" are displayed
-
-// Each player will be an object with:
-    // a name property with a value of the input previously written
-    // a deck property with a value an array of shuffled numbers from 2 to 14, each represented twice (2 Ace, 2 Kings, 2 Queens, etc...)
-    // a score property with a initial value at 0
-
-        //   const player1 = {
-        //        name: 'input',
-        //        deck: [4, 10, 9, 13, 8, 7, 4, 2, etc...],
-        //        score: 0
-        //      }
-
-// 2 empty variables will be initialized to store the value of the cars being played and compare them
-
-    // var card1, card2;
-
-// Each card will be identified by a positive value:    
-    // 2 of Spades = 2 
-    // 10 of Diamonds = 10
-    // Jacks = 11
-    // Queens = 12
-    // Kings = 13
-    // Aces = 14
-
-
-// ---------------------------------------------------------------------------------------------------------------------------
-// LOGIC 
-
-// The program will then wait for an event to be triggered:
-
-    // A click on the main deck of a player will grab the first value of the deck array and place it in a battle array
-    // When a player click once on its main deck, the click on the same deck is disabled until the other player click on his deck
-    // When the battle array has 2 values, it'll compare them and return the difference
-    // This difference will be assign as follow:
-
-        // Case 1 | => Player 1 - Player 2 return positive value | => Player 1 add the difference to his score
-        // Case 2 | => Player 1 - Player 2 return negative value | => Player 2 add the difference *-1 to his score
-        // Case 3 | => Player 2 - Player 1 return positive value | => Player 2 add the difference to his score
-        // Case 4 | => Player 2 - Player 1 return negative value | => Player 1 add the difference *-1 to his score
-
-// The code will run until both deck arrays are empty => then, both scores will be compare and the player object hosting the property of the highest score will display the value of the name property
+// full card deck (without the joker)
+const deck = [''];
 
 
 
+// _____________________________________________________________
+// VARIABLES 
 
 
-// ---------------------------------------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------------------------------------
+// 2 player objects, each including 3 properties: name, playing deck, and score
+const player1 = {};
+const player2 = {};
 
-// WAR GAME - Overpay version
+
+// an array that will host the deck array suffled
+var shuffleDeck = [];
+
+
+// an array that will host each player's shuffled deck, built from the shuffleDeck array
+var deck1 = [];
+var deck2 = [];
+
+
+// initialize variables that will store the value of the card played by both player each round
+var card1;
+var card2;
+
+
+
+// _____________________________________________________________
+// EVENT LISTENERS
+
+
+// 1 event listener for each player's input name's (little button to submit their name)
+    // enabled when the page is loaded
+    // disabled when a value is submitted
+
+
+// 1 event listener on a reset button
+    // reload the board game
+    // enable the name's submit buttons
+
+
+// 1 event listener to activate the game ("battle" button)
+    // enabled when both player name's input is entered
+    // trigger the init function
+
+
+// 1 event listener on each player's deck
+    // enabled when the "battle" button is triggered
+
+
+
+// _____________________________________________________________
+// FUNCTIONNALITIES
+
+
+// init function => initializing the board game
+    // triggered by the "battle" button
+    // invoke the deckShuffling function
+    // invoke the render function 
+
+
+// render function => rendering the board game
+    // called by init function
+    //
+    //
+    //
+
+
+// deckShuffling function => shuffling the deck array and build both shuffleDeck1 and shuffleDeck2
+    // called by init function
+    // assign both new arrays to both player's object
+
+
+// function clickPlayer1 => 
+    // triggered when player 1's deck is clicked
+    // add to the "battle field" div the first element (card) from the deck1 array
+    // remove the card from the deck1 array
+    // assign the parseInt value of the element to the card1 variable
+
+
+// function clickPlayer2 => 
+    // triggered when player 2's deck is clicked
+    // add to the "battle field" div the first element (card) from the deck2 array
+    // remove the card from the deck2 array
+    // assign the parseInt value of the element to the card2 variable
+
+
+// getScore function => calculate the difference between card1 and card2
+    // if card1 - card2 return positive value, add thst value to the score property of player 1 object
+    // if card1 - card2 return negative value, add this value *-1 to the score property of player 2 object
+
+
+// function displayScores => keep the score updated after each "battle"
+
+// function displayMessage => render a message of the round of the winner & the winner of the game at the end
+
 
