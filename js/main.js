e78422c6215db5bb9a2e983ab827ece754b30f45
Calvin Féau <calvin@calvins-mbp.lan>
2019-05-04 23:04:51 -0700

    finish details of logic + basic styling

--- js/main.js
+++ js/main.js
@@ -14,19 +14,17 @@ const deck = [
 // _____________________________________________________________
 // STATE VARIABLES 
 
-
 // 2 player objects, each including 3 properties: name, playing deck, and score
 var player1 = {
     name: 'Player 1'
 };
-
 var player2 = {
     name: 'Player 2'
 };
 
 // an array that will host the deck array suffled
-var shuffleDeck;
-var roundWinner;
+var shuffleDeck, roundWinner;
+
 // an array that will host each player's shuffled deck, built from the shuffleDeck array
 var deck1, deck2;
 
@@ -34,63 +32,72 @@ var deck1, deck2;
 var card1, card2;
 
 // _____________________________________________________________
+// CACHED ELEMENT REFERENCES
+
+// reset button and message
+const button = document.querySelector('#battlefield > button');
+const message = document.querySelector('#battlefield > #message');
+
+// player's inputs and titles
+const player1Input = document.querySelector('#input1');
+const player2Input = document.querySelector('#input2');
+const player1Title = document.querySelector('#player1 > h2');
+const player2Title = document.querySelector('#player2 > h2');
+const player1Form = document.querySelector('#player1 > form');
+const player2Form = document.querySelector('#player2 > form');
+
+// player's cards
+const player1Deck = document.querySelector("#player1 > div.card.back-blue");
+const player2Deck = document.querySelector("#player2 > div.card.back-red");
+const player1Card = document.querySelector('#battlefield > #cardPlayed > #card1');
+const player2Card = document.querySelector('#battlefield > #cardPlayed > #card2');
+
+// _____________________________________________________________
 // EVENT LISTENERS
 
 
-document.querySelector('#battlefield > #message').textContent = 'Enter your names if you want to fight!';
+// document.querySelector('#battlefield > #message').textContent = 'Enter your names if you want to fight!';
 
 document.querySelector('#nameBtn1').addEventListener('click', function(event) {
     event.preventDefault();
-    player1.name = document.querySelector('#input1').value;
-    document.querySelector('#player1 > h2').textContent = player1.name;
-    document.querySelector('#player1 > form').style.display = 'none';
-    return player1.name;
-});
+    player1Input.value !== '' ? player1.name = player1Input.value : player1.name;
+    player1Title.textContent = player1.name;
+    player1Title.style.display = 'block';
+    player1Form.style.display = 'none';
+    // return player1.name;
+});    
 
 document.querySelector('#nameBtn2').addEventListener('click', function(event) {
     event.preventDefault();
-    player2.name = document.querySelector('#input2').value;
-    document.querySelector('#player2 > h2').textContent = player2.name;
-    document.querySelector('#player2 > form').style.display = 'none';
-    return player2.name;
-});
-
-// 1 event listener to activate the game ("battle" button)
-    // enabled when both player name's input is entered
-    // trigger the init function
-
-document.querySelector('#battlefield > button').addEventListener('click', function() {
-    init();
-    document.querySelector('#battlefield > button').textContent = 'Reset';
-    console.log(deck1, deck2);
-});
-
+    player2Input.value !== '' ? player2.name = player2Input.value : player2.name;
+    player2Title.textContent = player2.name;
+    player2Title.style.display = 'block';
+    player2Form.style.display = 'none';
+    // return player2.name;
+});    
 
 // 1 event listener on a reset button
     // reload the board game
     // enable the name's submit buttons
+button.addEventListener('click', init);    
 
-
-// _____________________________________________________________
-// CACHED ELEMENT REFERENCES
 
 // _____________________________________________________________
 // FUNCTIONNALITIES
 
-// getScore function => calculate the difference between card1 and card2
-// if card1 - card2 return positive value, add thst value to the score property of player 1 object
-// if card1 - card2 return negative value, add this value *-1 to the score property of player 2 object
+init();
 
-function getScore() {
-    var roundScore = card1 - card2;
-    roundScore > 0 ? (player1.score += roundScore, roundWinner = player1.name) : (player2.score += (roundScore * -1), roundWinner = player2.name);
-    deckButtons();
-    displayScores();
+
+function displayWinner() {
+    player1.score > player2.score ? 
+    message.textContent = `${player1.name} won the war!`
+    : message.textContent = `${player2.name} won the war!`
 }
 
 // function displayMessage => render a message of the round of the winner & the winner of the game at the end
 function displayRoundWin () {
-    document.querySelector('#battlefield > #message').textContent = `${roundWinner} won the battle! But not the war...`;
+    deck1.length === 0 ? displayWinner () : roundWinner === 'tie' ? message.textContent = `It's a ${roundWinner}!` :
+    message.textContent = `${roundWinner} won the battle!`;
 }
 
 // function displayScores => keep the score updated after each "battle"
@@ -99,45 +106,56 @@ function displayScores() {
     document.querySelector('#player2Score').textContent = player2.score;
     displayRoundWin();
 }
+// getScore function => calculate the difference between card1 and card2
+// if card1 - card2 return positive value, add thst value to the score property of player 1 object
+// if card1 - card2 return negative value, add this value *-1 to the score property of player 2 object
+function getScore() {
+    var roundScore = card1 - card2;
+    roundScore === 0 ? roundWinner = 'tie' :
+    roundScore > 0 ? (player1.score += roundScore, roundWinner = player1.name) : (player2.score += (roundScore * -1), roundWinner = player2.name);
+    deckButtons();
+    displayScores();
+}
 
 function clickDeck1 () {
     // disable the click on the deck
-    document.querySelector("#player1 > div.card.back-blue").removeEventListener('click', clickDeck1);
+    player1Deck.removeEventListener('click', clickDeck1);
     
     // assign to card1 the first parseInt value of deck1
     card1 = parseInt(deck1[0].slice(1));
     
     // render the card on the battlefield
-    document.querySelector('#battlefield > #cardPlayed > #card1').setAttribute('class', `card ${deck1[0]}`);
+    player1Card.setAttribute('class', `card large ${deck1[0]}`);
     
     // shift this value from deck1
     deck1.shift();
     
     // check if player 2 played
     deck1.length === deck2.length ? 
-    getScore() : document.querySelector('#battlefield > #message').textContent = `${player2.name} turn`;
+    getScore() : message.textContent = `${player2.name} turn`;
 }
+
 function clickDeck2 () {
     // disable the click on the deck
-    document.querySelector("#player2 > div.card.back-red").removeEventListener('click', clickDeck2);
+    player2Deck.removeEventListener('click', clickDeck2);
     
     // assign to card1 the first parseInt value of deck1
     card2 = parseInt(deck2[0].slice(1));
     
     // render the card on the battlefield
-    document.querySelector('#battlefield > #cardPlayed > #card2').setAttribute('class', `card ${deck2[0]}`);
+    player2Card.setAttribute('class', `card large ${deck2[0]}`);
     
     // shift this value from deck2
     deck2.shift();
     
     // check if player 1 plauyed
     deck2.length === deck1.length ? 
-    getScore() : document.querySelector('#battlefield > #message').textContent = `${player1.name} turn`;    
+    getScore() : message.textContent = `${player1.name} turn`;    
 }
 
 function deckButtons () {
-    document.querySelector("#player1 > div.card.back-blue").addEventListener('click', clickDeck1);
-    document.querySelector("#player2 > div.card.back-red").addEventListener('click', clickDeck2);
+    player1Deck.addEventListener('click', clickDeck1);
+    player2Deck.addEventListener('click', clickDeck2);
 }
 
 // deckShuffling function => shuffling the deck array and build both shuffleDeck1 and shuffleDeck2
@@ -150,6 +168,20 @@ function deckShuffling() {
         (idx % 2 === 0) ? deck1.push(deck[i]) : deck2.push(deck[i]);
     });
     return deck1, deck2;
+}
+
+function startingBoard() {
+    message.textContent = 'please enter your name';
+    player1Card.setAttribute('class', 'card large');
+    player2Card.setAttribute('class', 'card large');
+
+    player1Input.value = '';
+    player1Form.style.display = 'block';
+    player1Title.style.display = 'none';
+
+    player2Input.value = '';
+    player2Form.style.display = 'block';
+    player2Title.style.display = 'none';
 }
 
 // init function => initializing the board game
@@ -171,4 +203,6 @@ function init() {
     
     // enable the event listeners on decks
     deckButtons();
+    displayScores();
+    startingBoard();
 }
