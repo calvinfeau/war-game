9d5f0834dac82fe92467cd990363d4e22c752095
Calvin Féau <calvin@Calvins-MacBook-Pro.local>
2019-05-09 12:44:59 -0700

    restyle game and add message display

--- js/main.js
+++ js/main.js
@@ -18,8 +18,8 @@ const deck = [
 var player1 = {
     name: 'Player 1'
 };
-var player2 = {
-    name: 'Player 2'
+var computer = {
+    name: 'Computer'
 };
 
 // initialize variables that will store the value of the card played by both player each round and the result of each round
@@ -31,26 +31,26 @@ var  roundWinner, card1, card2;
 
 // reset button and message
 const resetBtn = document.querySelector('.menu > #reset');
-const menuBtn = document.querySelector('.menu > #battleGame');
-const message = document.querySelector('#battlefield > #message');
+const menuBtn = document.querySelector('.menu > #home');
+const message = document.querySelector('.menu > #message');
 
 // player's inputs and titles
 const player1Input = document.querySelector('#input1');
-const player2Input = document.querySelector('#input2');
-const player1Title = document.querySelector('#player1 > div.scoreGame > h2');
-const player2Title = document.querySelector('#player2 > div.scoreGame > h2');
-const player1Form = document.querySelector('#player1 > form');
-const player2Form = document.querySelector('#player2 > form');
+// const player2Input = document.querySelector('#input2');
+const player1Title = document.querySelector('#info > #player1 > div.scoreGame > h2');
+const player2Title = document.querySelector('#info > #player2 > div.scoreGame > h2');
+const player1Form = document.querySelector('#info > #player1 > form');
+// const player2Form = document.querySelector('#player2 > form');
 
 // player's cards
-const player1Deck = document.querySelector("#player1 > div.card.back-blue");
-const player2Deck = document.querySelector("#player2 > div.card.back-red");
-const player1Card = document.querySelector('#battlefield > #card1');
-const player2Card = document.querySelector('#battlefield > #card2');
+const player1Deck = document.querySelector("#battlefield > #player1 > div.card.back-blue");
+const player2Deck = document.querySelector("#battlefield > #player2 > div.card.back-red");
+const player1Card = document.querySelector('#battlefield > #player1 > #card1');
+const player2Card = document.querySelector('#battlefield > #player2 > #card2');
 
 //player's score
-const player1Score = document.querySelector("#player1 > .scoreGame")
-const player2Score = document.querySelector("#player2 > .scoreGame")
+const player1Score = document.querySelector("#info > #player1 > .scoreGame")
+const player2Score = document.querySelector("#info > #player2 > .scoreGame")
 
 // _____________________________________________________________
 // EVENT LISTENERS
@@ -61,7 +61,7 @@ document.querySelector('#nameBtn1').addEventListener('click', player1Name);
 
 // reset button
 resetBtn.addEventListener('click', reset);    
-menuBtn.addEventListener('click', function() {location.reload()});
+menuBtn.addEventListener('click', introduction);
 
 // _____________________________________________________________
 // CREATE THE HOME PAGE
@@ -70,26 +70,35 @@ var introText = document.createElement('article');
 var scoreChoice = document.createElement('article');
 var battleChoice = document.createElement('article');
 
-document.body.appendChild(introText);
-document.body.appendChild(scoreChoice);
-document.body.appendChild(battleChoice);
-
 introduction();
 
 function introduction() {
+    document.querySelector('header > h1').textContent = 'WAR GAMES';
 
-introText.setAttribute('id', 'intro-text');
-scoreChoice.setAttribute('id', 'score-choice');
-battleChoice.setAttribute('id', 'battle-choice');
+    ((player1Score.getElementsByTagName('p').length > 0) && (player2Score.getElementsByTagName('p').length > 0)) ?
+    (document.querySelector('#player1 > .scoreGame > p').remove(), document.querySelector('#player2 > .scoreGame > p').remove())
+    : -1;
+    document.querySelector('main').setAttribute('style', 'display: none');
+    document.querySelector('nav').setAttribute('style', 'display: none');
 
-scoreChoice.addEventListener('click', scoreInit);
-battleChoice.addEventListener('click', battleInit);
+    document.body.appendChild(introText);
+    document.body.appendChild(scoreChoice);
+    document.body.appendChild(battleChoice);
 
-introText.textContent = 'Pick a gameplay and enjoy!';
-scoreChoice.innerHTML = '<span>SCORE</span><br><br>Get points to your score each round. <br>The highest score at the end of the battle wins!';
-battleChoice.innerHTML = "<span>BATTLE</span><br><br>Burn your opponent's extra cards <br> every time you win with a face card!'";
+    introText.setAttribute('id', 'intro-text');
+    scoreChoice.setAttribute('id', 'score-choice');
+    battleChoice.setAttribute('id', 'battle-choice');
 
-document.querySelector('nav').style.display = 'none';
+    introText.setAttribute('style', 'display: flex-inline');
+    scoreChoice.setAttribute('style', 'display: flex-inline');
+    battleChoice.setAttribute('style', 'display: flex-inline');
+
+    scoreChoice.addEventListener('click', scoreInit);
+    battleChoice.addEventListener('click', battleInit);
+
+    introText.textContent = 'Pick a gameplay and enjoy!';
+    scoreChoice.innerHTML = '<span>SCORE</span><br><br>Get points to your score each round. <br>The highest score at the end of the battle wins!';
+    battleChoice.innerHTML = "<span>BATTLE</span><br><br>Burn your opponent's extra cards <br> every time you win with a face card!";
 }
 // _____________________________________________________________
 // FUNCTIONNALITIES
@@ -99,7 +108,7 @@ document.querySelector('nav').style.display = 'none';
 
 // removes the home page and display the game page
 function removeIntroPage() {
-    introText.style.display = 'none';
+    introText.setAttribute('style', 'display: none');
     scoreChoice.setAttribute('style', 'display: none');
     battleChoice.setAttribute('style', 'display: none');
 
@@ -128,10 +137,10 @@ function cardReset() {
 // renders the name input
 function player2Name() {
     event.preventDefault();
-    player2Input.value !== '' ? player2.name = player2Input.value : player2.name;
-    player2Title.textContent = player2.name;
+    // player2Input.value !== '' ? player2.name = player2Input.value : player2.name;
+    player2Title.textContent = computer.name;
     player2Title.style.display = 'block';
-    player2Form.style.display = 'none';
+    // player2Form.style.display = 'none';
     player2Score.style.display = "flex";
 }
 function player1Name() {
@@ -156,7 +165,7 @@ function deckShuffling() {
         deck[randomIndex] = storingValue;
     }
     player1.deck = deck.slice(0, deck.length / 2);
-    player2.deck = deck.slice(deck.length / 2);
+    computer.deck = deck.slice(deck.length / 2);
 }
 
 // render the board after the player chose the game play
@@ -169,10 +178,9 @@ function startingBoard() {
     player1Form.style.display = 'block';
     player1Title.style.display = 'none';
     
-    player2Input.value = '';
-    player2Form.style.display = 'block';
+    // player2Input.value = '';
+    // player2Form.style.display = 'block';
     player2Title.style.display = 'none';
-    
     player1Score.style.display = "none";
     player2Score.style.display = "none";
     cardReset();
@@ -192,22 +200,21 @@ function player1WinsStyle() {
 
 // renders the message with the winner of the game
 function displayWinner() {
-    console.log(player1.deck, player2.deck);
     message.style.fontSize = "14px";
-    ((player1.score > player2.score) || (player2.deck.length === 0 && player1.deck.length !== 0)) ? 
+    ((player1.score > computer.score) || (computer.deck.length === 0 && player1.deck.length !== 0)) ? 
     message.textContent = `${player1.name} won the war!`
     : message.textContent = `Computer won the war!`; // change player2.name to 'computer'
 
     player1Deck.removeEventListener('click', clickDeck1);
-    player2Deck.removeEventListener('click', clickDeck2);
+    // player2Deck.removeEventListener('click', clickDeck2);
 
     player1Deck.removeEventListener('click', clickBattleDeck1);
-    player2Deck.removeEventListener('click', clickBattleDeck2);
+    // player2Deck.removeEventListener('click', clickBattleDeck2);
 }
 
 // renders the message with the winner of the round
 function displayRoundWin () {
-    (player1.deck.length === 0 || player2.deck.length === 0) ? 
+    (player1.deck.length === 0 || computer.deck.length === 0) ? 
     displayWinner() : roundWinner === 'tie' ? 
     (message.textContent = `It's a ${roundWinner}!`, player1Card.style.opacity = 0.3, player2Card.style.opacity = 0.3) :
     message.textContent = `${roundWinner} won the battle!`;
@@ -219,9 +226,10 @@ function displayRoundWin () {
 // launch the battle version
 function battleInit() {
     removeIntroPage();
+    document.querySelector('header > h1').textContent = 'BATTLE';
     createCardsLeftDisplay();
     player1.deck = [];
-    player2.deck = [];
+    computer.deck = [];
     deckShuffling();
     startingBoard();
     battleDeckButtons();
@@ -237,6 +245,7 @@ function battleDeckButtons() {
 // renders updated game board and checks, when both cards are on field, who's the round winner
 function clickBattleDeck1() {
     player1Name();
+    player2Name();
     cardReset();
     // if message.textcontent is telling a round winner, reset the board game
     (message.textContent === `It's a ${roundWinner}!` || message.textContent === `${roundWinner} won the battle!`) ? 
@@ -246,17 +255,18 @@ function clickBattleDeck1() {
     
     // assign to card1 the first parseInt value of deck1
     card1 = parseInt(player1.deck[0].slice(1));
+    card2 = parseInt(computer.deck[0].slice(1));
     
     // render the card on the battlefield
     player1Card.setAttribute('class', `card size ${player1.deck[0]}`);
+    player2Card.setAttribute('class', `card size ${computer.deck[0]}`);
     
-    clickBattleDeck2();
+    // clickBattleDeck2();
     getBattleScore();
     // player2Card.getAttribute('class') !== 'card size' ? 
     // getBattleScore() : message.textContent = `${player2.name}'s turn`;
 }
-function clickBattleDeck2() {
-    player2Name();
+// function clickBattleDeck2() {
     // cardReset();
     // if message.textcontent is telling a round winner, reset the board game
     // (message.textContent === `It's a ${roundWinner}!` || message.textContent === `${roundWinner} won the battle!`) ? 
@@ -265,19 +275,17 @@ function clickBattleDeck2() {
     // player2Deck.removeEventListener('click', clickBattleDeck2);
     
     // assign to card1 the first parseInt value of deck1
-    card2 = parseInt(player2.deck[0].slice(1));
     
     // render the card on the battlefield
-    player2Card.setAttribute('class', `card size ${player2.deck[0]}`);
     
     // player1Card.getAttribute('class') !== 'card size' ? 
     // getBattleScore() : message.textContent = `${player1.name}'s turn`;
-}
+// }
 
 // check the winner
 function getBattleScore() {   
     player1.deck.push(player1.deck.shift());
-    player2.deck.push(player2.deck.shift());
+    computer.deck.push(computer.deck.shift());
     var roundScore = card1 - card2;
     roundScore === 0 ? roundWinner = 'tie' :
     roundScore > 0 ? (player1WinsStyle(), player1WinsBattle()) : (player2WinsStyle(), player2WinsBattle());
@@ -288,19 +296,19 @@ function getBattleScore() {
 // compute the winner's gain
 function player2WinsBattle() {
     var cardLost = [];
-    card2 === 11 ? (cardLost = player1.deck.splice(0, 2), player2.deck = player2.deck.concat(cardLost)) :
-    card2 === 12 ? (cardLost = player1.deck.splice(0, 3), player2.deck = player2.deck.concat(cardLost)) :
-    card2 === 13 ? (cardLost = player1.deck.splice(0, 4), player2.deck = player2.deck.concat(cardLost)) : 
-    card2 === 14 ? (cardLost = player1.deck.splice(0, 5), player2.deck = player2.deck.concat(cardLost)) :
-    (cardLost = player1.deck.splice(0, 1), player2.deck = player2.deck.concat(cardLost));
+    card2 === 11 ? (cardLost = player1.deck.splice(0, 2), computer.deck = computer.deck.concat(cardLost)) :
+    card2 === 12 ? (cardLost = player1.deck.splice(0, 3), computer.deck = computer.deck.concat(cardLost)) :
+    card2 === 13 ? (cardLost = player1.deck.splice(0, 4), computer.deck = computer.deck.concat(cardLost)) : 
+    card2 === 14 ? (cardLost = player1.deck.splice(0, 5), computer.deck = computer.deck.concat(cardLost)) :
+    (cardLost = player1.deck.splice(0, 1), computer.deck = computer.deck.concat(cardLost));
 }
 function player1WinsBattle() {
     var cardLost = [];
-    card1 === 11 ? (cardLost = player2.deck.splice(0, 2), player1.deck = player1.deck.concat(cardLost)) :
-    card1 === 12 ? (cardLost = player2.deck.splice(0, 3), player1.deck = player1.deck.concat(cardLost)) :
-    card1 === 13 ? (cardLost = player2.deck.splice(0, 4), player1.deck = player1.deck.concat(cardLost)) : 
-    card1 === 14 ? (cardLost = player2.deck.splice(0, 5), player1.deck = player1.deck.concat(cardLost)) :
-    (cardLost = player2.deck.splice(0, 1), player1.deck = player1.deck.concat(cardLost));
+    card1 === 11 ? (cardLost = computer.deck.splice(0, 2), player1.deck = player1.deck.concat(cardLost)) :
+    card1 === 12 ? (cardLost = computer.deck.splice(0, 3), player1.deck = player1.deck.concat(cardLost)) :
+    card1 === 13 ? (cardLost = computer.deck.splice(0, 4), player1.deck = player1.deck.concat(cardLost)) : 
+    card1 === 14 ? (cardLost = computer.deck.splice(0, 5), player1.deck = player1.deck.concat(cardLost)) :
+    (cardLost = computer.deck.splice(0, 1), player1.deck = player1.deck.concat(cardLost));
 }
 
 // creates the counter of cards left for each player
@@ -310,14 +318,14 @@ function createCardsLeftDisplay() {
     player1Score.appendChild(score1);
     player2Score.appendChild(score2);
     score1.innerHTML = 'CARDS<br>LEFT';
-    score2.innerHTML = 'CARDS<br>LEFT'; 
+    score2.innerHTML = 'CARDS<br>LEFT';
 }
 
 // display updated amount of cards left for each player
 function displayCardsLeft() {
     document.querySelector('#player1Score').textContent = player1.deck.length;
-    document.querySelector('#player2Score').textContent = player2.deck.length;
-    (player1.deck.length == player2.deck.length) ? -1 : displayRoundWin();
+    document.querySelector('#player2Score').textContent = computer.deck.length;
+    (player1.deck.length == computer.deck.length) ? -1 : displayRoundWin();
 }
 
 
@@ -327,11 +335,12 @@ function displayCardsLeft() {
 function scoreInit() { 
     removeIntroPage();
     createScoreDisplay();
+    document.querySelector('header > h1').textContent = 'SCORE';
     player1.deck = [];
-    player2.deck = [];
+    computer.deck = [];
     deckShuffling();
     player1.score = 0;
-    player2.score = 0;
+    computer.score = 0;
     startingBoard();
     scoreDeckButtons();
     displayScores();
@@ -346,6 +355,7 @@ function scoreDeckButtons () {
 // renders updated game board and checks, when both cards are on field, who's the round winner
 function clickDeck1() {
     player1Name();
+    player2Name();
     cardReset();
     // if message.textcontent is telling a round winner, reset the board game
     (message.textContent === `It's a ${roundWinner}!` || message.textContent === `${roundWinner} won the battle!`) ? 
@@ -355,12 +365,15 @@ function clickDeck1() {
     
     // assign to card1 the first parseInt value of deck1
     card1 = parseInt(player1.deck[0].slice(1));
+    card2 = parseInt(computer.deck[0].slice(1));
     
     // render the card on the battlefield
     player1Card.setAttribute('class', `card size ${player1.deck[0]}`);
+    player2Card.setAttribute('class', `card size ${computer.deck[0]}`);
     
     // shift this value from player1.deck
     player1.deck.shift();
+    computer.deck.shift();
     
     clickDeck2();
     getScore();
@@ -369,7 +382,6 @@ function clickDeck1() {
     // getScore() : message.textContent = `${player2.name}'s turn`;
 }
 function clickDeck2() {
-    player2Name();
     // cardReset();
     // if message.textcontent is telling a round winner, reset the board game
     // (message.textContent === `It's a ${roundWinner}!` || message.textContent === `${roundWinner} won the battle!`) ? 
@@ -378,13 +390,10 @@ function clickDeck2() {
     // player2Deck.removeEventListener('click', clickDeck2);
     
     // assign to card1 the first parseInt value of player1.deck
-    card2 = parseInt(player2.deck[0].slice(1));
     
     // render the card on the battlefield
-    player2Card.setAttribute('class', `card size ${player2.deck[0]}`);
     
     // shift this value from player2.deck
-    player2.deck.shift();
     
     // check if player 1 played
     // player1Card.getAttribute('class') !== 'card size' ? 
@@ -393,11 +402,10 @@ function clickDeck2() {
 
 // calculates the difference between card1 and card2
 function getScore() {
-    console.log('working');
     var roundScore = card1 - card2;
     roundScore === 0 ? roundWinner = 'tie' :
     roundScore > 0 ? 
-    (player1.score += roundScore, player1WinsStyle()) : (player2.score += (roundScore * -1), player2WinsStyle());
+    (player1.score += roundScore, player1WinsStyle()) : (computer.score += (roundScore * -1), player2WinsStyle());
     scoreDeckButtons();
     displayScores();
 }
@@ -415,7 +423,7 @@ function createScoreDisplay() {
 
 // renders updated scores after each round
 function displayScores() {
-    player1.deck.length === 26 ? (player1.score = 0, player2.score = 0) : displayRoundWin();
+    player1.deck.length === 26 ? (player1.score = 0, computer.score = 0) : displayRoundWin();
     document.querySelector('#player1Score').textContent = player1.score;
-    document.querySelector('#player2Score').textContent = player2.score;
+    document.querySelector('#player2Score').textContent = computer.score;
 }
